79490fede3df40e09e99d097da023878d8387dde
Hossein <haghshenas3c@gmail.com>
2022-03-28 23:23:44 +0430

    Change sass to css

---
+++ practice/sass-variables/gulpfile.js
@@ -0,0 +1,12 @@
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+const buildStyle = () => {
+  return src("index.scss").pipe(sass()).pipe(dest("css"));
+};
+
+const watchTask = () => {
+  watch(["index.scss"], buildStyle);
+};
+
+exports.default = series(buildStyle, watchTask);
